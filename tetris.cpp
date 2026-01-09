37452eeb83bdea1a278cabc0059df010e2b52767
Ahnaf Tajwar Shayan <115848130+shaywu21@users.noreply.github.com>
2026-01-09 21:37:27 +0600

    added some features
    tetronimos, rotation logic, play area, terminal window size added

---
+++ tetris.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char *pField = nullptr;
+
+int nScreenWidth = 80; // Console Screen Size X (columns)
+int nScreenHeight = 30; // Console Screen Size Y (rows)
+
+int Rotate(int px, int py, int r) {
+    switch (r % 4) {
+        case 0: return py * 4 + px;             // 0 deg
+        case 1: return 12 + py - (4 * px);      // 90 deg
+        case 2: return 15 - (4 * py);           // 180 deg
+        case 3: return 3 + py + (4 * px);       // 270 deg
+    }
+    return 0;
+}
+
+int main() {
+    //creating assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L"..XX");
+    tetromino[6].append(L"..X.");
+    tetromino[6].append(L"..X.");
+
+    pField = new unsigned char[nFieldWidth*nFieldHeight]; // Create play area
+    for (int x = 0; x < nFieldWidth; x++)// Board Boundary
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y*nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+
+    wchar_t * screen = new wchar_t[nScreenWidth*nScreenHeight];
+    for (int i = 0; i < nScreenWidth*nScreenHeight; i++) screen[i] = L' ';
+}
